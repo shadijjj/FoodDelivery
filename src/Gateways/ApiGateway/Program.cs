3696c91ae2101a32c2d4c44fb2a3f6fdc7ad769b
shadijjj <shadijawad2004@gmail.com>
2026-08-19 10:17:14 +0300

    ui removal

--- src/Gateways/ApiGateway/Program.cs
+++ src/Gateways/ApiGateway/Program.cs
@@ -1,23 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.SetIsOriginAllowed(_ => true)
-              .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials();
-    });
-});
-
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
 var app = builder.Build();
 
 app.UseRouting();
-app.UseCors("AllowAll");
 app.MapReverseProxy();
 
 app.Run();
